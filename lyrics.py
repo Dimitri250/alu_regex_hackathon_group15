19b567213aa9c4e9ef4f4a58f1716f2b10c5fefe
Dimitri250 <118120317+Dimitri250@users.noreply.github.com>
2023-05-23 23:24:04 +0300

    lyrics.py

--- lyrics.py
+++ lyrics.py
@@ -1,5 +1,4 @@
 import re
-text =" iiiiiiiiiiiiiiiii [Verse 1] In the JUngle the mighty jungle the lion sleeps to night    [Verse 2]  In the Jungle the quite jungle the lion sleeps to night. "
+text ="  [Verse 1] In the JUngle the mighty jungle the lion sleeps to night    [Verse 2]  In the Jungle the quite jungle the lion sleeps to night. "
 pattern = r'\[Verse \d\].+'
 results = re.findall(pattern,text)
-print (results)
